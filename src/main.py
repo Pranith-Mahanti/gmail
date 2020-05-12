b102df0e87844dee3fa1fafd67a0f8feddafcd74
Pranith-Mahanti <63862712+Pranith-Mahanti@users.noreply.github.com>
2020-05-12 11:31:17 +0530

    Update main.py

--- src/main.py
+++ src/main.py
@@ -11,7 +11,7 @@ if os.path.isdir(path):
   pass
 else:
   os.system("mkdir ~/bin")
-  os.system("echo 'export PATH=$PATH":$HOME/bin'" >> ./.profile")
+  os.system("echo 'export PATH=$PATH\":$HOME/bin'" >> ./.profile")
 os.system("chmod +x "+f_name)
 os.system("mv {} {}".format(f_name, cmd))
 os.system("cp {} ~/bin".format(cmd))
