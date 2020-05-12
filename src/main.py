38bc7829475b771cbb241fda2cd236688528b847
Pranith-Mahanti <63862712+Pranith-Mahanti@users.noreply.github.com>
2020-05-12 11:27:34 +0530

    Create main.py

---
+++ src/main.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3.7
+import os
+import sys
+
+arg = sys.argv
+f_name = "mailto.py"
+cmd = "mailto"
+
+path = "~/bin"
+if os.path.isdir(path):
+  pass
+else:
+  os.system("mkdir ~/bin")
+  os.system("echo 'export PATH=$PATH":$HOME/bin"' >> ./.profile")
+os.system("chmod +x "+f_name)
+os.system("mv {} {}".format(f_name, cmd))
+os.system("cp {} ~/bin".format(cmd))
+
